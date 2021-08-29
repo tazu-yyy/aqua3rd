2bdcf3728448d4f9a8b3d844c94f9737ad58dafb
KongariBug <kongaribug@gmail.com>
2021-08-30 08:55:04 +0900

    Remove unused codes.

--- components/History.js
+++ components/History.js
@@ -1,26 +1,11 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import styles from '../styles/History.module.css'
-import YoutubeContainer from "./YoutubeContainer"
 import YoutubeContainers from "./YoutubeContainers"
-import {useState, useEffect} from "react"
 import SetList from "./SetList"
 import CustomVerticalTimelineElement from "./CustomVerticalTimelineElement"
 
 export default function History() {
-  const blueContentStyle = { background: 'rgb(68,191,228)', color: '#fff' }
-  const borderBlueContentStyle = { borderTop: '5px solid rgb(68,191,228)', color: '#000' }
-  const borderPinkContentStyle = { borderTop: '5px solid rgb(241,172,210)', color: '#000' }
-  const blueArrowStyle = { borderRight: '7px solid  rgb(68 ,191 ,228)' }
-  const pinkArrowStyle = { borderRight: '7px solid  rgb(241,172,210)' }
-  const whiteArrowStyle = { borderRight: '7px solid #aaa' }
-  const blueIconStyle = { background: 'rgb(68,191,228)', color: '#fff', display: 'flex', justifyContent: 'center' }
-  const pinkIconStyle = { background: 'rgb(241,172,210)', color: '#fff', display: 'flex', justifyContent: 'center' }
-  const anchorIcon = <img style={{ width: '75%'}} src='anchor.svg' />
-  const noteIcon = <img style={{ width: '60%'}} src='note01.svg' />
-  const noteIcon2 = <img style={{ width: '60%'}} src='note02.svg' />
-  // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
   const monthToPrefix = (month) => {
     switch (month) {
       case 1:
