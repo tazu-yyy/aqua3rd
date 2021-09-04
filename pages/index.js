e90ce97153f4446eeeefde89474183c4ee48de0c
KongariBug <kongaribug@gmail.com>
2021-09-04 19:46:22 +0900

    Change to English.

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,12 @@ export default function Home() {
       <Head>
         <title>#湊あくあ３年目の軌跡（非公式ファンサイト）</title>
         <meta name="twitter:card" content="summary" key={"twitter_card"} />
-        <meta property="og:url" content="https://www.湊あくあ.moe" key={"og_url"}/>
+        <meta property="og:url" content="https://www.xn--l8ja2b6546b.moe" key={"og_url"}/>
         <meta property="og:type" content="website" key={"og_type"}/>
         <meta property="og:title" content="#湊あくあ３年目の軌跡（非公式ファンサイト）" key={"og_title"}/>
         <meta property="og:description" content="#湊あくあ３年目の軌跡（非公式ファンサイト） ホロライブ所属バーチャルアイドルゲーマー猫耳メイドの湊あくあ、３年目の歴史を振り返ろう" key={"og_description"}/>
         <meta property="og:site_name" content="#湊あくあ３年目の軌跡（非公式ファンサイト）" key={"og_site_name"}/>
-        <meta property="og:image" content="https://www.湊あくあ.moe/images/fav.png" key={"og_image"}/>
+        <meta property="og:image" content="https://www.xn--l8ja2b6546b.moe/images/fav.png" key={"og_image"}/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
